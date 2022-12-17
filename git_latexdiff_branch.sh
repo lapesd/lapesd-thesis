79c73a76c02e3ecf5ec39ed54c2384d889bf616e
Alexis Armin Huf <alexishuf@gmail.com>
2022-12-17 15:20:22 -0300

    Improve	safety/usability of git_latexdiff_branch.sh
    - Print a help message if user did not provide a git branch or commit
    - Use the script dir as	the working directory and as `repo_path`
    - Guess	main latex file	as Makefile does, but allows `old_main_file`/`new_main_file` to	be set	if names changed accross commits
    - Use mktemp to create a temp dir instead of hardcoded tmp. Maybe someone has a not so temporary tmp dir
    - Halt execution if git archive or tar xzvf fail

--- git_latexdiff_branch.sh
+++ git_latexdiff_branch.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
-repo_path=.
-old_main_file=main.tex
-new_main_file=main.tex
-temporary_dir=$repo_path/tmp
-mkdir $temporary_dir
+# This script should be placed alongside the Makefile
+repo_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "$repo_path"
+
+# Guess main latex file as the .tex file starting with \documentclass.*{lapesd-thesis}. Fallback to main.tex
+basename=$((grep -rE '\\documentclass.*lapesd-thesis' | cut -d : -f 1 | grep -E '.tex$' | xargs -n 1 bash -c 'echo $(echo $@ | wc -c):$@' a | sed -E 's/^([0-9]):/00\1:/' | sed -E 's/^([0-9][0-9]):/0\1:/' | sort -u | head -n 1 | sed -E 's/^[0-9]+:(.*).tex$/\1/g' > basename.make.log && test "$$(cat basename.make.log)" != "001:" && cat basename.make.log && rm -f basename.make.log)  || echo main)
+
+# Maybe the main file changed accross commits. In that case, call me like so:
+# old_main_file=old_name.tex new_main_file=new_name.tex ./git_latexdiff_branch.sh COMMIT
+old_main_file=${old_main_file:-$basename.tex}
+new_main_file=${new_main_file:-$basename.tex}
+
+# Use mktemp to create a temp dir inside $repo_path. Using a random name avoids clashes and allows
+# the rm -fr at the end to be safe
+temporary_dir=$(mktemp -d "$repo_path/latexdiff.XXXX")
+
+# Check the user provided an input commit. git archive will check if it is valid.
+if [ -z "$1" ]; then
+  echo "Usage: $0 git_brach_or_commit" >&2
+  exit 1
+fi
 
 #Git archive does not compress submodules!
-git archive --format=tar.gz $1 > $temporary_dir/old_project.tar.gz
-tar -xzvf $temporary_dir/old_project.tar.gz -C $temporary_dir/
-rm $temporary_dir/old_project.tar.gz
+git archive --format=tar.gz $1 > $temporary_dir/old_project.tar.gz || exit 1
+tar -xzvf $temporary_dir/old_project.tar.gz -C $temporary_dir/ || exit 1
+rm $temporary_dir/old_project.tar.gz || exit 1
 
 #These flags are for: highlight type; consider "include" files; consider
 #citations spaces; consider citations marks; and put everything from every file
@@ -25,5 +41,5 @@ mv $repo_path/diff.pdf $temporary_dir/
 make MAIN_FILE=diff clean
 mv $temporary_dir/diff.pdf $repo_path/
 
-rm -r $temporary_dir
-rm diff.tex
+rm -fr $temporary_dir
+rm -f diff.tex
